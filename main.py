d3359fe1ac6e6415e9c591356faf71eae1a89add
mehmet sonmez <gate3510@gmail.com>
2023-11-02 16:27:27 -0400

    Button icons added. UI style options added. UI optimized

--- main.py
+++ main.py
@@ -23,52 +23,79 @@ class StudyTimer:
         self.root.iconbitmap(resource_path("profile.ico"))
         # Set the window title
         self.root.title("Study Timer - Designed by Mehmet")
+        # UI options and fonts
+        paddings = {'padx': 5, 'pady': 10}
+        clock_font = {'font': ('Monospace', 64)}
+        study_label = {'font': ('Monospace', 22)}
+        note_label = {'font': ('Monospace', 12)}
+        # Icons
+        start_icon = tk.PhotoImage(file='./start.png')
+        skip_icon = tk.PhotoImage(file='./skip.png')
+        reset_icon = tk.PhotoImage(file='./reset.png')
 
-        self.s = ttk.Style()
-        self.s.configure("Color1.TFrame", background="green")
-        self.s.configure("Color2.TFrame", background="blue")
-        self.s.configure("Color3.TFrame", background="red")
-        self.s.configure("TNoteBook.Tab", font=("Ubuntu", 20))
-        self.s.configure("TButton.Tab", font=("Ubuntu", 20))
-        self.s.configure("TButton", background = "blue", bd = 10, foreground = "black", relief="raised", borderwidth=1, focusthickness=3, focuscolor="none")
+        self.style = ttk.Style()
+        self.style.configure("Color1.TFrame", background="green")
+        self.style.configure("Color2.TFrame", background="blue")
+        self.style.configure("Color3.TFrame", background="red")
+        self.style.configure("TNoteBook.Tab")
+        self.style.configure("TButton.Tab")
+        self.style.configure("TButton",
+                         background = "blue",
+                         relief="raised",
+                         borderwidth=1, bd = 10, foreground = "black", focusthickness=3, focuscolor="none")
 
-        self.tabs = ttk.Notebook(self.root)
+        # Tabs
+        self.tabs = ttk.Notebook(self.root, style="TNoteBook.Tab")
         self.tabs.pack(fill="both", expand=False)
 
         self.tab1 = ttk.Frame(self.tabs, width=400, height=60, style="Color1.TFrame")
         self.tab2 = ttk.Frame(self.tabs, width=400, height=60, style="Color2.TFrame")
         self.tab3 = ttk.Frame(self.tabs, width=400, height=60, style="Color3.TFrame")
 
-        # Tabs Labels
         self.tabs.add(self.tab1, text="Study")
         self.tabs.add(self.tab2, text="Short Break")
         self.tabs.add(self.tab3, text="Long Break")
 
         # Time Labels
-        self.study_timer_label = ttk.Label(self.tab1, text="25:00", font=("Ubuntu", 48))
+        self.study_timer_label = ttk.Label(self.tab1, text="25:00", **clock_font)
         self.study_timer_label.pack()
-        self.short_break_timer_label = ttk.Label(self.tab2, text="05:00", font=("Ubuntu", 48))
+        self.short_break_timer_label = ttk.Label(self.tab2, text="05:00", **clock_font)
         self.short_break_timer_label.pack()
-        self.long_break_timer_label = ttk.Label(self.tab3, text="15:00", font=("Ubuntu", 48))
+        self.long_break_timer_label = ttk.Label(self.tab3, text="15:00", **clock_font)
         self.long_break_timer_label.pack()
 
+        # Grid
         self.grid_layout = ttk.Frame(self.root)
         self.grid_layout.pack(pady=20)
 
         # Start Button
-        self.start_button = ttk.Button(self.grid_layout, text="Start", command=self.start_timer_thread)
-        self.start_button.grid(row=0, column=0)
+        self.start_button = ttk.Button(self.grid_layout,
+                                       image=start_icon,
+                                       text="Start",
+                                       compound=tk.LEFT,
+                                       style="TButton",
+                                       command=self.start_timer_thread)
+        self.start_button.grid(row=0, column=0, **paddings)
 
         # Skip Button
-        self.skip_button = ttk.Button(self.grid_layout, text="Skip", command=self.skip_clock)
-        self.skip_button.grid(row=0, column=1)
+        self.skip_button = ttk.Button(self.grid_layout,
+                                      image=skip_icon,
+                                      text="Skip",
+                                      compound=tk.LEFT,
+                                      command=self.skip_clock)
+        self.skip_button.grid(row=0, column=1, **paddings)
 
         # Reset Button
-        self.reset_button = ttk.Button(self.grid_layout, text="Reset", command=self.reset_clock)
-        self.reset_button.grid(row=0, column=2)
+        self.reset_button = ttk.Button(self.grid_layout,
+                                      image=reset_icon,
+                                      text="Reset",
+                                      compound=tk.LEFT,
+                                      command=self.reset_clock)
+        self.reset_button.grid(row=0, column=2, **paddings)
 
-        self.study_counter_label = ttk.Label(self.grid_layout, text="Study Points: 0", font=("Ubuntu", 16))
-        self.study_counter_label.grid(row=1, column=0, columnspan=3, pady=10)
+        # Study Point Label
+        self.study_counter_label = ttk.Label(self.grid_layout, text="Study Points: 0", **study_label)
+        self.study_counter_label.grid(row=1, column=0, columnspan=3, **paddings)
 
         self.points = 0
         self.skipped = False
@@ -77,18 +104,19 @@ class StudyTimer:
 
         # Load Button
         self.load_button = ttk.Button(self.grid_layout, text="Load", command=self.load_note)
-        self.load_button.grid(row=4, column=0)
+        self.load_button.grid(row=2, column=1)
 
         # Save Button
         self.save_button = ttk.Button(self.grid_layout, text="Save", command=self.save_note)
-        self.save_button.grid(row=4, column=2)
+        self.save_button.grid(row=2, column=2)
 
         # Textbox
-        self.textbox_label = tk.Label(self.grid_layout, text="Leave a Note", font=("Ubuntu", 12), justify="center")
-        self.textbox_label.grid(row=2, column=1, pady=10)
+        notes = tk.StringVar()
+        self.textbox_label = tk.Label(self.grid_layout, text="Notebook", **note_label, justify="left")
+        self.textbox_label.grid(row=2, column=0, **paddings)
 
-        self.textbox = tk.Text(self.root, height=6, font=("Ubuntu", 12))
-        self.textbox.pack(padx=15, pady=10)
+        self.textbox = tk.Text(self.root, height=6, **note_label)
+        self.textbox.pack(**paddings)
 
         self.root.mainloop()
 
@@ -102,7 +130,7 @@ class StudyTimer:
         selected_file = filedialog.asksaveasfile(parent=self.root, defaultextension=".txt", filetypes=[("Text file",".txt"),("All files","*")])
         if selected_file is None:
             return
-        filetext = str(self.textbox.get(1.0,5.0))
+        filetext = str(self.textbox.get(1.0,6.0))
         selected_file.write(filetext)
         selected_file.close()
     
